1fd3a269ed6f6a7f799a9d6af29f68c37c071b85
Horllymoble <horlamidex1@gmail.com>
2020-02-19 15:49:45 -0800

    Fift commit

--- src/main.js
+++ src/main.js
@@ -1,11 +1,19 @@
 import Vue from 'vue'
 import App from './App.vue'
 
-import { MdButton, MdDrawer} from 'vue-material/dist/components'
+
+import VueResource from 'vue-resource';
+
+import { MdButton, MdDrawer,MdCard} from 'vue-material/dist/components'
 import 'vue-material/dist/vue-material.min.css'
+
+Vue.use(VueResource)
+
+Vue.http.options.root = 'https://dominoo-9fa87.firebaseio.com/'
 
 Vue.use(MdButton)
 Vue.use(MdDrawer)
+Vue.use(MdCard)
 
 new Vue({
   el:"#app",
